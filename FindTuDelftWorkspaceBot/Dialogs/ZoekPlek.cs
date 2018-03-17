174ebfa260a4414f3cb61ca06b8257af882de53d
Velthoven van, Joost <JVL@qdelft.nl>
2018-03-17 16:36:58 +0100

    Add project files.

---
+++ FindTuDelftWorkspaceBot/Dialogs/ZoekPlek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace FindTuDelftWorkspaceBot.Dialogs
+{
+
+    [Serializable]
+    public class SandwichOrder
+    {
+
+        public int NumberOfRequieredComputers { get; set; }
+
+        public static IForm<SandwichOrder> BuildForm()
+        {
+            return new FormBuilder<SandwichOrder>()
+                    .Message("Welcome to the simple sandwich order bot!")
+                    
+                    .OnCompletion(OnComplete)
+                    .Build();
+        }
+
+        private static async Task<IDialogContext> OnComplete(IDialogContext context, SandwichOrder state)
+        {
+
+            await context.PostAsync("Welkom bij QDelft :)");
+
+
+            return context;
+        }
+    }
+}
---
+++ FindTuDelftWorkspaceBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FindTuDelftWorkspaceBot.WebApiApplication" Language="C#" %>
