b2088df29567507f69c06097e0c85f4cb7df7b7e
Velthoven van, Joost <JVL@qdelft.nl>
2018-03-22 15:37:12 +0100

    check in

--- ApiTestConsole/Program.cs
+++ ApiTestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using TuDelft.Api;
 
 namespace ApiTestConsole
@@ -12,13 +13,28 @@ namespace ApiTestConsole
             string name = Console.ReadLine();
             Console.WriteLine($"Welcome {name}!");
 
+            Console.WriteLine("What is your building of choice?");
+            string bulding = Console.ReadLine();
 
-            var items = TuDelftWorkspace.Get();
+            Console.WriteLine("How many computers do you need?");
+            int requestedComputers = Convert.ToInt32(Console.ReadLine());
+
+            //TU rest api will be managed by this code.
+            var items = TuDelftWorkspace.Get()
+                       .Take(30)
+                       .Where(a => a.Location.Contains(bulding))
+                       .Where(a => a.NumberOfAvailableComputers >= requestedComputers)
+                       .ToList();
+                
             foreach(var item in items)
             {
                 Console.WriteLine($"{item.Location} - {item.NumberOfAvailableComputers}");
             }
 
+            if(!items.Any())
+            {
+                Console.WriteLine("No rooms found");
+            }
             Console.ReadLine();
 
         }
--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -11,27 +11,58 @@ namespace FindTuDelftWorkspaceBot.Dialogs
     {
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync("Welke locatie zoekt u!");
+            await context.PostAsync("Welcome welcome welcome! What is your name?");
             context.Wait(MessageReceivedAsync);
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var inputMessage = await result as Activity;
-            string location;
 
-            context.UserData.TryGetValue<string>("Name", out location);
 
-            if(string.IsNullOrEmpty(location))
+
+            context.UserData.TryGetValue<string>("Name", out string name);
+
+            if (string.IsNullOrEmpty(name))
             {
                 context.UserData.SetValue<string>("Name", inputMessage.Text);
-                location = inputMessage.Text;
+                name = inputMessage.Text;
+                await context.PostAsync("What is your building of choice?");
             }
 
-            TuDelftWorkspace.Get()
-                .Take(10)
-                .ToList()
-                .ForEach(async place => await context.PostAsync($"{place.Location} -  {place.NumberOfAvailableComputers}"));
+
+            context.UserData.TryGetValue<string>("bulding", out string bulding);
+            if (string.IsNullOrEmpty(bulding))
+            {
+                context.UserData.SetValue<string>("bulding", inputMessage.Text);
+                bulding = inputMessage.Text;
+
+                await context.PostAsync("How many computers do you need?");
+
+            }
+
+            context.UserData.TryGetValue<int>("requestedComputers", out int requestedComputers);
+            if (string.IsNullOrEmpty(name))
+            {
+
+                if (int.TryParse(inputMessage.Text, out int parsedInput))
+                {
+                    context.UserData.SetValue<int>("requestedComputers", parsedInput);
+                    await context.PostAsync("Please wait :)");
+
+                    TuDelftWorkspace.Get()
+                       .Take(30)
+                       .Where(a => a.Location.Contains(bulding))
+                       .Where(a => a.NumberOfAvailableComputers >= parsedInput)
+                       .ToList()
+                       .ForEach(async place => await context.PostAsync($"{place.Location} -  {place.NumberOfAvailableComputers}"));
+
+                }
+
+
+            }
+
+
 
         }
     }
